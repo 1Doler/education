ff170ab84c85473c7de86e4b969aea1b64c52b5e
daler <kabilovd7@gmail.com>
2023-09-03 21:43:39 +0300

    TASK-6: add routing and lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { Suspense } from "react";
+import { Route, Routes, Link } from "react-router-dom";
 import { Counter } from "./components/Counter"
+
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPageAsync";
+
 import "./index.scss";
 
 export const App = () => {
     return <div className="app">
-        REACT
+        <Link to={"/"}>Главная</Link>
+        <Link to={"/about"}>О сайте</Link>
+            
+        <Suspense fallback={<div>Load</div>}>
+            <Routes>    
+                <Route path="/about" element={<AboutPageAsync />} />
+                <Route path="/" element={<MainPageAsync />}/>     
+            </Routes>
+        </Suspense>
         <Counter />
     </div>
 }
