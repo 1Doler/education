bfedf4945374b4cfb1e444b5e0bda601a5acb7b4
daler <kabilovd7@gmail.com>
2023-10-17 15:34:06 +0300

    fsd

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -20,7 +20,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
         module: {
             rules: buildLoaders(options)
         },
-        resolve: buildResolvers(),
+        resolve: buildResolvers(options),
         devtool: isDev ? "inline-source-map" : undefined, //Указывает где была сделана ошибка
         devServer: isDev ? buildDevServer(options) : undefined
     }
